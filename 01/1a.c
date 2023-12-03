41baf2bc5d1276949c01fe60cfd870866c00b7f9
TheUltimateOptimist <digitalConfidence@web.de>
2023-12-03 11:48:00 +0100

    finished_day_1

--- 01/1a.c
+++ 01/1a.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+void replace(char * input, const char * from, char to) {
+    int start = 0;
+    int fromLength = strlen(from);
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] == from[start]) {
+            if (start == fromLength - 1) {
+                input[i - 1] = to;
+                start = 0;
+            }
+            else {
+                start++;
+            }
+        }
+        else if (input[i] == from[0]){
+            start = 1;
+        }
+        else {
+            start = 0;
+        }
+    }
+}
 
 int main() {
     FILE *filePointer;
@@ -10,6 +33,16 @@ int main() {
     }
     int sum = 0;
     while(fgets(line, sizeof(line), filePointer) != NULL) {
+        printf("%s", line);
+        replace(line, "nine", '9');
+        replace(line, "eight", '8');
+        replace(line, "seven", '7');
+        replace(line, "six", '6');
+        replace(line, "five", '5');
+        replace(line, "four", '4');
+        replace(line, "three", '3');
+        replace(line, "two", '2');
+        replace(line, "one", '1');
         printf("%s", line);
         char firstNumber = 'a';
         char secondNumber = 'a';
--- 01/test.c
+++ 01/test.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
-void replace(char * text, char * new) {
-    printf("%lu", sizeof(text));
-    printf("%lu", strlen(text));
-    str
-    printf("%lu", sizeof(new));
-    printf("%s", new);
+void replace(char * input, const char * from, char to) {
+    int start = 0;
+    int fromLength = strlen(from);
+    printf("%s %d\n", from, fromLength);
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] == from[start]) {
+            if (start == fromLength - 1) {
+                input[i - 1] = to;
+                start = 0;
+            }
+            else {
+                start++;
+            }
+        }
+        else if (input[i] == from[0]){
+            start = 1;
+        }
+        else {
+            start = 0;
+        }
+    }
 }
 
 int main() {
-    char text[5] = "hallo";
-    replace("hallo", "some");
+    char line[] = "83hqrd1sixsevennine\n";
+    printf("%s", line);
+    replace(line, "nine", '9');
+        replace(line, "eight", '8');
+        replace(line, "seven", '7');
+        replace(line, "six", '6');
+        replace(line, "five", '5');
+        replace(line, "four", '4');
+        replace(line, "three", '3');
+        replace(line, "two", '2');
+        replace(line, "one", '1');
+        printf("%s", line);
+    printf("%s", line);
 }
