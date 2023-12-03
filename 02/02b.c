bacd03c85876951476dcb61d3fa943677b2cb6ee
TheUltimateOptimist <digitalConfidence@web.de>
2023-12-03 14:31:47 +0100

    completed_day_2

---
+++ 02/02b.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+
+int power(int base, int exp) {
+    if (exp == 0) {
+        return 1;
+    }
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result = result * base;
+    } 
+    return result;
+}
+
+int gamePow(char * line, int beginning, int maxRed, int maxGreen, int maxBlue) {
+    int minRed = 1;
+    int minGreen = 1;
+    int minBlue = 1;
+    char number[5];
+    int numberIndex = 0;
+    for (int i = beginning; i < strlen(line); i++) {
+        if (line[i] >= '0' && line[i] <= '9') {
+            number[numberIndex] = line[i];
+            numberIndex++;
+        }
+        if ((line[i] == 'r' || line[i] == 'g' || line[i] == 'b') && numberIndex > 0) {
+            int numberValue = 0;
+            for (int j = numberIndex - 1; j >= 0; j--) {
+                numberValue = numberValue + (number[numberIndex - 1 - j] - 48)*power(10, j);
+            }
+            printf("%c:%d ", line[i], numberValue);
+            switch (line[i]) {
+                case 'r': {
+                    if (numberValue > minRed) {
+                        minRed = numberValue;
+                    }
+                };
+                break;
+                case 'g': {
+                    if (numberValue > minGreen) {
+                        minGreen = numberValue;
+                    }
+                }
+                break;
+                case 'b': {
+                    if (numberValue > minBlue) {
+                        minBlue = numberValue;
+                    }
+                }
+            }
+            numberIndex = 0;
+        }
+    }
+    return minRed * minGreen * minBlue;
+}
+
+int numberOfDigits(int number) {
+    int digits = 0;
+    while(number != 0) {
+        digits++;
+        number = number / 10;
+    }
+    return digits;
+}
+
+int main() {
+    const int maxRed = 12;
+    const int maxGreen = 13;
+    const int maxBlue = 14;
+
+    FILE *filePointer;
+    char line[200];
+    filePointer = fopen("adventofcode.com_2023_day_2_input.txt", "r");
+    if (filePointer == NULL) {
+        printf("Error opening the file!\n");
+        return 1;
+    }
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+        fgets(line, sizeof(line), filePointer);
+        sum += gamePow(line, 7 + numberOfDigits(i), maxRed, maxGreen, maxBlue);
+    }
+    printf("%d\n", sum);
+}
