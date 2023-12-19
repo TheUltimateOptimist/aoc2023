82776738acd4ada2bc7ccb45b57cffe5f0cce0c0
TheUltimateOptimist <digitalConfidence@web.de>
2023-12-19 22:04:10 +0100

    corrected_day_18

--- 18/18.py
+++ 18/18.py
@@ -1,4 +1,4 @@
-from util import read
+from util import read, analyze_polygon
 
 entries = read().split("\n")
 for i in range(len(entries)):
@@ -6,7 +6,7 @@ for i in range(len(entries)):
 
 directions: dict[str, tuple[int, int]] = {"R": (0, 1), "L": (0, -1), "U": (-1, 0), "D": (1, 0)}
 
-path = [(0, 0)]
+path: list[tuple[int, int]] = [(0, 0)]
 
 outer = 0
 current = (0, 0)
@@ -17,13 +17,4 @@ for entry in entries:
     current = (current[0] + count*direction[0], current[1] + count*direction[1])
     path.append(current)
 
-a = 0
-b = 0
-for i in range(len(path) - 1):
-    a += path[i][0]*path[i+1][1]
-for i in range(1, len(path)):
-    b += path[i][0]*path[i-1][1]
-
-area = 0.5*abs(a - b)
-inner = 1 + area - outer / 2
-print(f"result = {round(outer + inner)}")
+print(f"result = {analyze_polygon(path).bothside}")
--- 18/18b.py
+++ 18/18b.py
@@ -1,4 +1,4 @@
-from util import read
+from util import read, analyze_polygon
 
 entries = read().split("\n")
 for i in range(len(entries)):
@@ -6,7 +6,7 @@ for i in range(len(entries)):
 
 directions: dict[int, tuple[int, int]] = {0: (0, 1), 2: (0, -1), 3: (-1, 0), 1: (1, 0)}
 
-path = [(0, 0)]
+path: list[tuple[int, int]] = [(0, 0)]
 
 outer = 0
 current = (0, 0)
@@ -17,16 +17,4 @@ for entry in entries:
     current = (current[0] + count*direction[0], current[1] + count*direction[1])
     path.append(current)
 
-
-# shoelace forumula or gausche trapezformel
-a = 0
-b = 0
-for i in range(len(path) - 1):
-    a += path[i][0]*path[i+1][1]
-for i in range(1, len(path)):
-    b += path[i][0]*path[i-1][1]
-
-# pick's theorem
-area = 0.5*abs(a - b)
-inner = 1 + area - outer / 2
-print(f"result = {round(outer + inner)}")
+print(f"result = {analyze_polygon(path).bothside}")
