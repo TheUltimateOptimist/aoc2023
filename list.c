c2dd6cb09d79d5fae8ca33b3e50a16d3fb9ee6c4
TheUltimateOptimist <digitalConfidence@web.de>
2024-01-10 23:28:44 +0100

    added_list_macro

--- list.c
+++ list.c
@@ -3,6 +3,33 @@
 #include <string.h>
 #include <math.h>
 #define INITIALSIZE 8
+#define LIST(type, name) \
+    typedef struct name##_list {\
+        size_t length;\
+        size_t capacity;\
+        size_t elementSize;\
+        type *data;\
+        void (*add)(struct name##_list self, type *value);\
+        void (*set)(struct name##_list self, int at, type *value);\
+        type (*get)(struct name##_list self, int at);\
+        type (*pop)(struct name##_list self);\
+        type (*popAt)(struct name##_list self, int at);\
+        void (*clear)(struct name##_list self);\
+    } name##_list;\
+    name##_list name##_list_create() {\
+        name##_list new_list;\
+        new_list.length = 0;\
+        new_list.capacity = INITIALSIZE;\
+        new_list.elementSize = sizeof(type);\
+        new_list.data = malloc(INITIALSIZE*sizeof(type));\
+        new_list.add = (void (*)(name##_list self, type *value)) listAdd;\
+        new_list.set = (void (*)(name##_list self, int at, type *value)) listSet;\
+        new_list.get = (type (*)(name##_list self, int at)) listGet;\
+        new_list.pop = (type (*)(name##_list self)) listPop;\
+        new_list.popAt = (type (*)(name##_list self, int at)) listPopAt;\
+        new_list.clear = (void (*)(name##_list self)) listClear;\
+        return new_list;\
+    }
 
 //list implementation with resizing arrays (insertion in O(1) amortized)
 typedef struct list {
