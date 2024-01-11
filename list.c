667146ff2700f91e640feaf96f00573a3d2f2d23
TheUltimateOptimist <digitalConfidence@web.de>
2024-01-11 22:57:21 +0100

    changed_list

--- list.c
+++ list.c
@@ -9,12 +9,6 @@
         size_t capacity;\
         size_t elementSize;\
         type *data;\
-        void (*add)(struct name##_list self, type *value);\
-        void (*set)(struct name##_list self, int at, type *value);\
-        type (*get)(struct name##_list self, int at);\
-        type (*pop)(struct name##_list self);\
-        type (*popAt)(struct name##_list self, int at);\
-        void (*clear)(struct name##_list self);\
     } name##_list;\
     name##_list name##_list_create() {\
         name##_list new_list;\
@@ -22,13 +16,54 @@
         new_list.capacity = INITIALSIZE;\
         new_list.elementSize = sizeof(type);\
         new_list.data = malloc(INITIALSIZE*sizeof(type));\
-        new_list.add = (void (*)(name##_list self, type *value)) listAdd;\
-        new_list.set = (void (*)(name##_list self, int at, type *value)) listSet;\
-        new_list.get = (type (*)(name##_list self, int at)) listGet;\
-        new_list.pop = (type (*)(name##_list self)) listPop;\
-        new_list.popAt = (type (*)(name##_list self, int at)) listPopAt;\
-        new_list.clear = (void (*)(name##_list self)) listClear;\
         return new_list;\
+    }\
+    list name##_list_raw(name##_list *self) {\
+        list result;\
+        result.length = self->length;\
+        result.capacity = self->capacity;\
+        result.elementSize = self->elementSize;\
+        result.data = self->data;\
+        return result;\
+    }\
+    void name##_list_add(name##_list *self, type value) {\
+        list selfl = name##_list_raw(self);\
+        listAdd(&selfl, &value);\
+        self->length = selfl.length;\
+        self->capacity = selfl.length;\
+        self->data = selfl.data;\
+    }\
+    void name##_list_set(name##_list *self, int at, type value) {\
+        list selfl = name##_list_raw(self);\
+        listSet(&selfl, at, &value);\
+    }\
+    type name##_list_get(name##_list *self, int at) {\
+        list selfl = name##_list_raw(self);\
+        type *result = listGet(&selfl, at);\
+        return *result;\
+    }\
+    type name##_list_pop(name##_list *self) {\
+        list selfl = name##_list_raw(self);\
+        type *result = listPop(&selfl);\
+        self->length = selfl.length;\
+        self->capacity = selfl.length;\
+        self->data = selfl.data;\
+        return *result;\
+    }\
+    type name##_list_popat(name##_list *self, int at) {\
+        list selfl = name##_list_raw(self);\
+        type *result = listPopAt(&selfl, at);\
+        self->length = selfl.length;\
+        self->capacity = selfl.length;\
+        self->data = selfl.data;\
+        return *result;\
+    }\
+    void name##_list_clear(name##_list *self) {\
+        list selfl = name##_list_raw(self);\
+        listClear(&selfl);\
+        self->length = selfl.length;\
+        self->capacity = selfl.length;\
+        self->data = selfl.data;\
     }
 
 //list implementation with resizing arrays (insertion in O(1) amortized)
