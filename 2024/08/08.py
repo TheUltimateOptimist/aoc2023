7dd8e6e692398af181145227dbbcc89b4b2e9b0e
TheUltimateOptimist <digitalConfidence@web.de>
2024-12-23 01:54:49 +0100

    beautified day 8

--- 2024/08/08.py
+++ 2024/08/08.py
@@ -1,30 +1,24 @@
-from util import read
+from util import read, Grid, pair
+from itertools import combinations 
 
-grid = read().strip().splitlines()
-frequencies: dict[str, list[tuple[int, int]]] = dict()
-for i, row in enumerate(grid):
-    for j, col in enumerate(row):
-        if col != '.':
-            positions = frequencies.get(col)
-            if positions is not None:
-                positions.append((i, j))
-            else:
-                frequencies[col] = [(i, j)]
+grid = Grid.from_input(read())
+frequencies: dict[str, list[pair]] = dict()
+for pos, value in grid:
+    if value == '.': continue
+    positions = frequencies.get(value)
+    if positions is not None:
+        positions.append(pos)
+    else: frequencies[value] = [pos]
 
-antinodes: set[tuple[int, int]] = set()
-for frequency in frequencies:
-    antennas = frequencies.get(frequency)
-    for a in range(len(antennas) - 1):
-        for b in range(a + 1, len(antennas)):
-            first = antennas[a]
-            second = antennas[b]
-            firstd = (first[0] - second[0], first[1] - second[1])
-            secondd = (second[0] - first[0], second[1] - first[1])
-            while first[0] >= 0 and first[0] < len(grid) and first[1] >= 0 and first[1] < len(grid[0]):
-                antinodes.add(first)
-                first = (first[0] + firstd[0], first[1] + firstd[1])
-            while second[0] >= 0 and second[0] < len(grid) and second[1] >= 0 and second[1] < len(grid[0]):
-                antinodes.add(second)
-                second = (second[0] + secondd[0], second[1] + secondd[1]) 
+antinodes = set()
+for frequency, antennas in frequencies.items():
+    for first, second in combinations(antennas, 2):
+        diff = first - second
+        while first in grid:
+            antinodes.add(first)
+            first = first + diff 
+        while second in grid:
+            antinodes.add(second)
+            second = second - diff 
 
 print(len(antinodes))
