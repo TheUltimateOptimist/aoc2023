514d54b76f385a3c62f5e2146557b57cc332b69d
TheUltimateOptimist <digitalConfidence@web.de>
2024-12-26 03:02:29 +0100

    solved day 4

---
+++ 2015/04/04.py
@@ -0,0 +1,11 @@
+import hashlib
+
+input = "iwrupvqb"
+value = 0
+while True:
+    value += 1
+    in_bytes = f"{input}{value}".encode('utf-8')
+    hash = hashlib.md5(in_bytes)
+    if hash.hexdigest().startswith('00000'):
+        print(value)
+        break
