1854f78094a494286ee56257d62995e85b7737b3
TheUltimateOptimist <digitalConfidence@web.de>
2024-12-30 23:26:23 +0100

    solved 2025 day 10

---
+++ 2015/10/10b.cpp
@@ -0,0 +1,29 @@
+#include "../../util.hpp"
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+string next_str(const string& input) {
+    string result;
+    int count = 0;
+    for (int a = 0; a < input.length(); a++) {
+        count++;
+        if (a == input.length() - 1 || input[a] != input[a + 1]) {
+            result += to_string(count);
+            result += input[a];
+            count = 0;
+        }
+    }
+    return result;
+}
+
+int main(int argc, char** argv) {
+    string input = read(2015, 10, argc);
+    for (int i = 0; i < 50; i++) {
+        input = next_str(input); 
+        cout << input << " " << input.length() << endl;
+    }
+}
