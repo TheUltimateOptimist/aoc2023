bb7923eb62455eb27fb1411db0230e22949712b0
TheUltimateOptimist <digitalConfidence@web.de>
2025-01-03 19:30:28 +0100

    solved 2015 day 16

---
+++ 2015/16/16b.cpp
@@ -0,0 +1,37 @@
+#include "../../util.hpp"
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    unordered_map<string, int> expected = {
+        {"children:", 3},
+        {"cats:", 7},
+        {"samoyeds:", 2},
+        {"pomeranians:", 3},
+        {"akitas:", 0},
+        {"vizslas:", 0},
+        {"goldfish:", 5},
+        {"trees:", 3},
+        {"cars:", 2},
+        {"perfumes:", 1}
+    };
+    string input = read(2015, 16, argc);
+    for (auto line : split(input, "\n")) {
+        vector<string> parts = split(line, " ");
+        for (int i = 2; i <= 6; i += 2) {
+            if (parts[i] == "cats:" || parts[i] == "trees:") {
+                if (atoi(parts[i + 1].c_str()) <= expected[parts[i]]) break;
+            }
+            else if (parts[i] == "pomeranians:" || parts[i] == "goldfish:") {
+                if (atoi(parts[i + 1].c_str()) >= expected[parts[i]]) break;
+            }
+            else if (expected[parts[i]] != atoi(parts[i + 1].c_str())) break;
+            if (i == 6) {
+                cout << line << endl;
+            }
+        }
+    }
+}
